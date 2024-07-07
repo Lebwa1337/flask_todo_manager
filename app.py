9219337bdfc1b01fed162350827b3c2662724f14
LeBwa <lebwa1337@gmail.com>
2024-07-07 20:08:08 +0300

    Crud and index page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -19,9 +19,46 @@ class Todo(db.Model):
         return self.text
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        try:
+            db.session.add(Todo(text=task_content))
+            db.session.commit()
+            return redirect("/")
+        except Exception:
+            return "There was an error"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:pk>/')
+def delete(pk):
+    task_to_delete = Todo.query.get_or_404(pk)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except Exception:
+        return "There was an error"
+
+
+@app.route('/update/<int:pk>/', methods=['GET', 'POST'])
+def update(pk):
+    task = Todo.query.get_or_404(pk)
+    if request.method == 'POST':
+        task.text = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception:
+            return "There was an error with updating"
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == '__main__':
